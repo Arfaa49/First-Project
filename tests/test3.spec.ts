d05156a12bd77320cd8f0dc92910601a4c35edb4
Arfaa49 <ranaarfa864@gmail.com>
2026-01-05 15:51:26 +0000

    Initial Playwright project

---
+++ tests/test3.spec.ts
@@ -0,0 +1,56 @@
+import{ test, expect } from '@playwright/test'
+ 
+ 
+ 
+ 
+test.only('@Webst Client App login', async ({ page }) => {
+   const email = "anshika@gmail.com";
+   const productName = 'ZARA COAT 3';
+   const products = page.locator(".card-body");
+   await page.goto("https://rahulshettyacademy.com/client");
+   await page.locator("#userEmail").fill(email);
+   await page.locator("#userPassword").fill("Iamking@000");
+   await page.locator("[value='Login']").click();
+   await page.waitForSelector("h5:has-text('ZARA COAT 3')")
+   //await page.waitForLoadState('networkidle');
+   await page.locator(".card-body b").first().waitFor();
+   const titles = await page.locator(".card-body b").allTextContents();
+   console.log(titles); 
+   const count = await products.count();
+   for (let i = 0; i < count; ++i) {
+      if (await products.nth(i).locator("b").textContent() === productName) {
+         //add to cart
+         await products.nth(i).locator("text= Add To Cart").click();
+         break;
+      }
+   }
+ 
+   await page.locator("[routerlink*='cart']").click();
+   //await page.pause();
+ 
+   await page.locator("div li").first().waitFor();
+   const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+   expect(bool).toBeTruthy();
+   await page.locator("text=Checkout").click();
+ 
+  await page.getByPlaceholder('Select Country').pressSequentially("ind", { delay: 150 }) 
+   const dropdown = page.locator(".ta-results");
+   await dropdown.waitFor();
+   const optionsCount = await dropdown.locator("button").count();
+   for (let i = 0; i < optionsCount; ++i) {
+      const text = await dropdown.locator("button").nth(i).textContent();
+      if (text === " India") {
+         await dropdown.locator("button").nth(i).click();
+         break;
+      }
+   }
+ 
+   expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+   await page.locator(".action__submit").click();
+   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   const orderId = await page.locator(".ng-star-inserted").first().textContent();
+   console.log(orderId);
+   await page.locator("button[routerlink*='myorders']").click();
+
+});
+ 
